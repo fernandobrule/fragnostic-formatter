534589a60d7b4dc81c514c682397f409460c8782
Fernando Brûlé <fernandobrule@gmail.com>
2021-06-03 08:51:38 -0300

    format mobile

--- fragnostic-formatter/src/main/scala/com/fragnostic/formatter/support/MobileFormatter.scala
+++ fragnostic-formatter/src/main/scala/com/fragnostic/formatter/support/MobileFormatter.scala
@@ -2,11 +2,16 @@ package com.fragnostic.formatter.support
 
 trait MobileFormatter {
 
-  private val mobileNumberRegex = """^\d{13}$"""
+  // 5511951976773 => +55 (11) 951976773
+  private val mobileNumberWithCountryCodeRegex = """^\d{13}$"""
+  // 11951976773 => (11) 951976773
+  private val mobileNumberWithoutCountryCodeRegex = """^\d{11}$"""
 
   def format(mobile: String): String =
-    if (mobile.matches(mobileNumberRegex)) {
+    if (mobile.matches(mobileNumberWithCountryCodeRegex)) {
       s"\u002b${mobile.substring(0, 2)}\u0020\u0028${mobile.substring(2, 4)}\u0029\u0020${mobile.substring(4)}"
+    } else if (mobile.matches(mobileNumberWithoutCountryCodeRegex)) {
+      s"\u0028${mobile.substring(0, 2)}\u0029\u0020${mobile.substring(2)}"
     } else {
       mobile
     }
